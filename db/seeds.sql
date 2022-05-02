5232de17bba56c19aae304fafe46d4f7934ade2d
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-05-02 18:16:36 -0500

    testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,7 @@ VALUES
 ('Engineering'),
 ('Finance');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Sales person', 30000, 1),
 ('Sales Manager', 35000, 1),
@@ -15,7 +15,7 @@ VALUES
 ('Accountant Manager', 55000, 3);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
 ('Cameron', 'Stroup', 2, null),
 ('Hunter', 'Stroup', 4, null),
@@ -23,7 +23,7 @@ VALUES
 ('Trent', 'Stroup', 1, 1),
 ('Susan', 'Current', 1, 1),
 ('Hil', 'Stroup', 3, 2),
-('Collen', 'Williams', 3, 2),
+('Colleen', 'Stroup', 3, 2),
 ('Lake', 'Goodboy', 1, 1),
 ('Christain', 'Current', 5, 3),
 ('Tiffany', 'Stroup', 5, 3);
--- index.js
+++ index.js
@@ -1,2 +1,80 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "mycompanyDb",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// const mainMenu = () => {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "list",
+//         name: "question",
+//         message: "What would you like to do?",
+//         choices: [
+//           "Examine Departments",
+//           "Examine Roles",
+//           "Examine Employees",
+//           "Add Department",
+//           "Add Role",
+//           "Add Employee",
+//           "Update Employees",
+//         ],
+//       },
+//     ])
+//     .then((answer) => {
+//       if (answer.question === "Add Department") {
+//         addDepartment();
+//       } else if (answer.question === "Add Employees") {
+//         employeeQuestions();
+//       } else if (answer.question === "Add Role") {
+//         roleQuestions();
+//       } else if (answer.question === "Add Department") {
+//         roleQuestions();
+//       } else if (answer.question === "Examine Departments") {
+//         ExamineDepartments();
+//       } else if (answer.question === "Examine Roles") {
+//         ExamineRoles();
+//       } else if (answer.question === "Examine Employees") {
+//         ExamineEmployees1();
+//       } else if (answer.question === "Update Employee Role") {
+//         updateEmployeeRoles();
+//       } else {
+//         connection.end();
+//       }
+//     });
+// };
+
+// // const addDepartment= ()=> {
+// //     inquirer.prompt([
+// //         {
+// //             type: 'input',
+// //             name: "deparment",
+// //             message: "Enter the name of the daparment you want to add"
+
+// //         }
+// //     ])
+// //     .then(answers=> {
+// //         const deptQuerry= INSERT INTO deparment (deparment)VALUES(`${answers.deparment}`))
+// //     }
+
+// // }
+
+// // mainMenu();
